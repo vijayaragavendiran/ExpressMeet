19fef8a11380aef2ee4ef9021bfe99f2bb8ca4ce
vijayaragavendiran <vijayaragavendiran@outlook.com>
2016-03-06 19:19:31 -0800

    Meet up home completed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.locals.appdata = require('./data.json');
 app.use('/', routes);
 
 
