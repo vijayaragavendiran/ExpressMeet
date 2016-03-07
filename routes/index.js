8106555c41cf3a771dc417447c43474cec761450
vijayaragavendiran <vijayaragavendiran@outlook.com>
2016-03-07 10:44:07 -0800

    completed speakers

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index' });
 });
+
+// GET Speakers page
+router.get('/speakers',function(req,res){
+  res.render('speakers',{title : 'Speakers'});
+})
 
 module.exports = router;
